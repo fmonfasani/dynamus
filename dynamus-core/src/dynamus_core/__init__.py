8eedf9263614298e3bdb17b396974ac022ba14b3
Federico Monfasani <67231410+fmonfasani@users.noreply.github.com>
2025-08-20 15:29:11 -0300

    fix(core): add package initializer

---
+++ dynamus-core/src/dynamus_core/__init__.py
@@ -0,0 +1,17 @@
+from .framework import (
+    FieldDefinition,
+    EntityDefinition,
+    GenerationContext,
+    CodeTemplate,
+    CodeGenerator,
+    TemplateBasedGenerator,
+)
+
+__all__ = [
+    "FieldDefinition",
+    "EntityDefinition",
+    "GenerationContext",
+    "CodeTemplate",
+    "CodeGenerator",
+    "TemplateBasedGenerator",
+]
